983e999231f9365d51aa1834201b81468932b22b
vasilij korobov <kavyabass51@gmail.com>
2024-05-31 01:09:36 +0700

    Добавлена функция lower_chair() Исправленна докстринга upper_chair()

--- str_func.py
+++ str_func.py
@@ -1,7 +1,11 @@
 def upper_chair(text):
-    """НЕ ЗНАЯ ЧТО ДОК СТРИНГА УЖЕ ЕСТЬ ДОБАВЛЯЕМ"""
+    """ДЕЛАЕТ ВСЕ БУКВЫ БОЛЬШИМИ"""
     return text.upper()
 
 def capitalize_first_chair(text):
     """Эта Функция Делает Заглавной Первую Букву Каждого Слова Текста"""
     return text.capitalize()
+
+def lower_chair(text):
+    """делает все буквы мелкими"""
+    return text.lower()
