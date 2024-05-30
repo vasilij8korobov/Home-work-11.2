556ec3e7f678ec4125836c353179eeb1e4a6e6aa
vasilij korobov <kavyabass51@gmail.com>
2024-05-31 00:48:26 +0700

    Добавляем функцию capitalize_first_chair() А так же докстрингу к функции upper_chair()

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def upper_chair(text):
+    """НЕ ЗНАЯ ЧТО ДОК СТРИНГА УЖЕ ЕСТЬ, ДОБАВЛЯЕМ"""
     return text.upper()
+
+def capitalize_first_chair(text):
+    """Эта Функция Делает Заглавной Первую Букву Каждого Слова Текста"""
+    return text.capitalize()
